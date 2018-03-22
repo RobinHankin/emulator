93a78c7af5ed827c07a74d7187f8fc5cd3862550
RobinHankin <hankin.robin@gmail.com>
2018-03-23 09:32:41 +1300

    initial upload

---
+++ data/expert.estimates.R
@@ -0,0 +1,27 @@
+if(FALSE){
+"expert.estimates" <-
+structure(list(low = c(1.3, 2500, 5.261e-05, 2, 2800000, 1318000, 
+0.9, 0.0174, 0.06, 0.06, 0.943, 5.05, 0.8, 3200, 0.719, 1354), 
+    best = c(1.7, 4100, 1.824e-05, 3.4, 3800000, 1585000, 1.3, 
+    0.0692, 0.14, 0.14, 1, 5.77, 0.85, 6200, 0.906, 1368), high = c(2.1, 
+    5700, 6.325e-05, 4.8, 4800000, 1905000, 1.7, 0.275, 0.22, 
+    0.22, 1.057, 6.49, 0.9, 9200, 1.094, 1382)), .Names = c("low", 
+"best", "high"), class = "data.frame", row.names = c("windstress", 
+"oc.horiz.diffus", "oc.vert.diffus", "oc.drag", "at.heat.diffus", 
+"at.mois.diffus", "at.width", "at.slope", "advfact.zonalmois", 
+"advfact.meridmois", "co2.scaling", "clim.sens", "thres.humid", 
+"ice.diffus", "fw.scaling", "solar.const"))
+}
+
+"expert.estimates" <- 
+  structure(list(
+                  low = c(1.3, 2500, 5.261e-05, 2.0, 2800000, 1318000, 0.9, 0.0174, 0.06, 0.06, 0.957, 1, 0.80, 3200, 0.719, 1354),
+                 best = c(1.7, 4100, 1.824e-05, 3.4, 3800000, 1585000, 1.3, 0.0692, 0.14, 0.14, 1.000, 5, 0.85, 6200, 0.906, 1368),
+                 high = c(2.1, 5700, 6.325e-05, 4.8, 4800000, 1905000, 1.7, 0.2750, 0.22, 0.22, 1.059, 10, 0.9, 9200, 1.094, 1382)),
+            .Names = c("low", "best", "high"),
+            class = "data.frame",
+            row.names = c("windstress", 
+              "oc.horiz.diffus", "oc.vert.diffus", "oc.drag", "at.heat.diffus", 
+              "at.mois.diffus", "at.width", "at.slope", "advfact.zonalmois", 
+              "advfact.meridmois", "co2.scaling", "clim.sens", "thres.humid", 
+              "ice.diffus", "fw.scaling", "solar.const"))
