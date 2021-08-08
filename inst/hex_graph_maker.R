98688edc7a440b84c0c13bcb2c726363b7b84268
Robin Hankin <hankin.robin@gmail.com>
2021-08-08 12:04:16 +1200

    tweaks for visual appearance

--- inst/hex_graph_maker.R
+++ inst/hex_graph_maker.R
@@ -1,4 +1,5 @@
-func <- function(x){out <- c(1,x)
+library("emulator")
+`func` <- function(x){out <- c(1,x)
     names(out)[1] <- "const"
     return(out)
 }
@@ -9,24 +10,22 @@ colnames(toy) <- "a"
 rownames(toy) <- paste("obs",1:nrow(toy),sep=".")
 
 beta <- c(-1,1)
-fish <- 10
+fish <- 20
 A <- corr.matrix(toy, scales=fish)
 Ainv <- solve(A)
 set.seed(0)
 d.mean <- apply(regressor.multi(toy),1,function(x){sum(beta*x)})
 d <- c(rmvnorm(n=1,mean=d.mean, sigma=A*0.1))
-
-plot(c(toy),d)
 x <- seq(from=0,to=1,len=100)
 
 jj <- interpolant.quick(as.matrix(x), d, toy, scales=fish,
                         func=func, 
                         Ainv=Ainv,g=TRUE)
 
-plot(x,jj$mstar.star,xlim=c(0,1),ylim=c(-1,1),type="l",col="black",lwd=3,axes=FALSE,xlab="",ylab="")
-lines(x,jj$prior,col="green",type="l")
-lines(x,jj$mstar.star+2*jj$Z,type="l",col="red",lty=2)
-lines(x,jj$mstar.star-2*jj$Z,type="l",col="red",lty=2)
+pdf(file="emulator_icon.pdf")
+plot(x,jj$mstar.star,xlim=c(0,1),ylim=c(-1,1),type="l",col="black",lwd=5,axes=FALSE,xlab="",ylab="")
+lines(x,jj$prior,type="l",lwd=2)
+lines(x,jj$mstar.star+1.95*jj$Z,type="l",lty=2,lwd=3)
+lines(x,jj$mstar.star-1.95*jj$Z,type="l",lty=2,lwd=3)
 points(toy,d,pch=16,cex=2)
-
-
+dev.off()
